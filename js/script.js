3821fb01e001f517d3efa719055f099be9d4a2b5
natan.rocha <natan.paularocha@gmail.com>
2023-08-05 01:40:38 -0300

    developed all desk and mobile page layout and stylized

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-/* Coloque o JS neste arquivo */
+$(document).ready(function() {
+  $('.carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
+    prevArrow: $('.custom-prev'),
+    nextArrow: $('.custom-next'),
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          className: "slider variable-width",
+          arrows: false,
+          centerMode: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true
+        }
+      }
+    ]
+  });
+});
